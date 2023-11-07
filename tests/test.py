1e2c3c4a20e98d85bfe5d7395489ffb03d27f563
ElToro1966 <mail@ericeikrem.com>
2023-11-07 16:59:22 +0100

    Add unit tests

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import unittest
+from main import path_to_cwd
+import os
+
+
+class ServerHeartbeatTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
