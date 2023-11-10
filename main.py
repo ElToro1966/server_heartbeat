516a8cdaea218ce3424240d44b8401bc597c1f46
ElToro1966 <mail@ericeikrem.com>
2023-11-10 12:47:34 +0100

    Add functions for reading config and creating log file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 import configparser
 import logging
+import logging.handlers
 import asyncio
 import os
 import time
@@ -17,38 +18,52 @@ def path_to_cwd(filename):
     return os.path.join(root_path, filename)
 
 
-config_file = path_to_cwd("config.ini")
-config = configparser.ConfigParser()
-try:
-    config.read(config_file)
-except OSError:
-    print("Config file not found, exiting.")
-    exit(1)
+def get_config(config_file):
+    config = configparser.ConfigParser()
+    try:
+        config.read(path_to_cwd(config_file))
+    except OSError:
+        print("Config file not found, exiting.")
+        exit(1)
+    return config
 
-log_file = config["logging"]["log_file"]
-info_message = config["logging"]["info_message"]
-error_message = config["logging"]["error_message"]
-down_check_message = config["logging"]["down_check_message"]
-servers = config["servers"].items()
-logging.basicConfig(filename=log_file, encoding='utf-8', level=logging.INFO)
+
+def create_rotating_log_file(log_file, log_file_max_bytes, log_file_backup_count):
+    server_logger = logging.getLogger("ServerLogger")
+    server_logger.setLevel(logging.INFO)
+    handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=int(log_file_max_bytes),
+                                                   backupCount=int(log_file_backup_count))
+    server_logger.addHandler(handler)
+
+
+config_file = get_config("config.ini")
+log_file = config_file["logging"]["log_file"]
+log_file_max_bytes = int(config_file["logging"]["log_file_max_bytes"])
+log_file_backup_count = int(config_file["logging"]["log_file_backup_count"])
+create_rotating_log_file(log_file, log_file_max_bytes, log_file_backup_count)
+
+info_message = config_file["logging"]["info_message"]
+error_message = config_file["logging"]["error_message"]
+down_check_message = config_file["logging"]["down_check_message"]
+servers = config_file["servers"].items()
 
 
 def log_status(server, response):
     message = str(time.ctime()) + " Checking server: " + server.name + " at address: " \
               + server.address + " with wait time: " + str(server.wait) + " seconds. "
     if response == 200:
-        logging.info(message + server.name + " - All is well.")
+        logging.info(message + server.name + " - " + info_message)
     else:
-        logging.error(message + server.name + " is down!")
+        logging.error(message + server.name + " - " + error_message)
 
 
 async def fail_check(server):
     while True:
         if server.status() == 200:
-            logging.info(info_message + server.name + " is back up!")
+            logging.info(server.name + " - " + info_message)
             break
         else:
-            logging.error(error_message + server.name + " is still down!")
+            logging.error(server.name + " - " + down_check_message)
         await asyncio.sleep(server.down_check_interval)
 
 
@@ -56,6 +71,7 @@ async def server_up_check(server):
     while True:
         current_server = httpserver.HttpServer(server)
         log_status(current_server, current_server.status())
+        print(current_server.status())
         if current_server.status() != 200:
             fail_check(current_server)
         await asyncio.sleep(current_server.wait)
